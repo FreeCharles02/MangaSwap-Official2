870df254972d3b411f6549fb2adf8eb1227becc3
Charles <105826694+FreeCharles02@users.noreply.github.com>
2024-06-14 18:37:36 -0500

    Update cart.jsx

--- pages/user-pages/cart.jsx
+++ pages/user-pages/cart.jsx
@@ -1,2 +1,16 @@
 import React from 'react'
 
+const cart = () => {
+    return (
+        <div>
+            <h2>
+                Your cart is empty 
+            </h2>
+            <p>
+                Shop for Manga volumes and more...
+            </p>
+        </div>
+    )
+}
+
+export default cart;
